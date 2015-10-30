78135c280b5ea72383ff6fc9d55f76134982e81d
孤独的√3 <596392912@qq.com>
2015-10-30 18:16:52 +0800

    update

--- src/main/java/net/dreamlu/ui/beetl/AssetsTag.java
+++ src/main/java/net/dreamlu/ui/beetl/AssetsTag.java
@@ -1,11 +1,18 @@
 package net.dreamlu.ui.beetl;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.beetl.core.GeneralVarTagBinding;
 
+import com.jfinal.kit.StrKit;
+
+import net.dreamlu.kit.AssetsKit;
+
 /**
+ * <#assets file="">
  * 
+ * <#/assets>
  */
 public class AssetsTag extends GeneralVarTagBinding {
 	
@@ -13,10 +20,21 @@ public class AssetsTag extends GeneralVarTagBinding {
 	@SuppressWarnings("unchecked")
 	public void render() {
 		Map<String, Object> paras = (Map<String, Object>) this.args[1];
-		Object type = paras.get("type");
-		Object list = paras.get("list");
-		
-		
+		Object file = paras.get("file");
+		if (file == null) {
+			throw new RuntimeException("assets tag attribute file can not be null" );
+		}
+		String fileName = file.toString();
+		if (StrKit.isBlank(fileName)) {
+			throw new RuntimeException("assets tag attribute file can not be null" );
+		}
+		try {
+			String src = AssetsKit.combo(fileName);
+			this.binds(src);
+			this.doBodyRender();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
